76d76b869f38685c9cb13d6b3c45a0d8bc300729
Peter Arnold <peter@deepsixdesign.com>
2020-01-26 13:37:30 -0500

    asd

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,3 +1,4 @@
+mkdir datasets
 FILE=$1
 
 if [[ $FILE != "ae_photos" && $FILE != "apple2orange" && $FILE != "summer2winter_yosemite" &&  $FILE != "horse2zebra" && $FILE != "monet2photo" && $FILE != "cezanne2photo" && $FILE != "ukiyoe2photo" && $FILE != "vangogh2photo" && $FILE != "maps" && $FILE != "cityscapes" && $FILE != "facades" && $FILE != "iphone2dslr_flower" && $FILE != "ae_photos" ]]; then
@@ -5,11 +6,10 @@ if [[ $FILE != "ae_photos" && $FILE != "apple2orange" && $FILE != "summer2winter
     exit 1
 fi
 
-mkdir ./input
 URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
-ZIP_FILE=./input/$FILE.zip
-TARGET_DIR=./input/$FILE/
+ZIP_FILE=./datasets/$FILE.zip
+TARGET_DIR=./datasets/$FILE/
 wget -N $URL -O $ZIP_FILE
 mkdir $TARGET_DIR
-unzip $ZIP_FILE -d ./input/
+unzip $ZIP_FILE -d ./datasets/
 rm $ZIP_FILE
